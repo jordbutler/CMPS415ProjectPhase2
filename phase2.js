fea42429e1e5ae865985190648d99cac3a2e4c84
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-23 03:40:29 -0500

    Update phase2.js

--- phase2.js
+++ phase2.js
@@ -2,10 +2,18 @@ const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://jordbutler:AcidKvvng253@phase2.s5v65hg.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 const mongoose = require("mongoose");
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
     
     app.get('/', function(req, res) {
-        
+      var outstring = 'Starting... ';
+      res.send(outstring);
         
         mongoose.connect(uri).then(() => {
     console.log("Connected to MongoDB");
@@ -18,6 +26,5 @@ const mongoose = require("mongoose");
 
 
 
-main().catch(console.error);
-
 // Add functions that make DB calls here
+
