4bff904ece7cb725608fcbd21165afd4006f0a39
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-23 03:32:43 -0500

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,32 +1,22 @@
 const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://jordbutler:AcidKvvng253@phase2.s5v65hg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+const mongoose = require("mongoose");
 
-async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://jordbutler:AcidKvvng253@phase2.s5v65hg.mongodb.net/?retryWrites=true&w=majority";
     
-    /**
-     * The Mongo Client you will use to interact with your database
-     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
-     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
-     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
-     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
-     */
-    const client = new MongoClient(uri);
+    app.get('/', function(req, res) {
+        
+        
+        mongoose.connect(uri).then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+        
+ });
 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
 
-        // Make the appropriate DB calls
-
-    } finally {
-        // Close the connection to the MongoDB cluster
-        await client.close();
-    }
-}
 
 main().catch(console.error);
 
