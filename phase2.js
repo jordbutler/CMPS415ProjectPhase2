5f212003d5a0194d5ef1b987598f5188bac528cc
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-23 02:25:23 -0500

    Update phase2.js

--- phase2.js
+++ phase2.js
@@ -1,45 +1,49 @@
-const { MongoClient } = require("mongodb");
-
-// The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://jordbutler:AcidKvvng253@cmps415phase2.2zmcyxj.mongodb.net/?retryWrites=true&w=majority";
-
-// --- This is the standard stuff to get it to work on the browser
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+const bodyParser = require("body-parser");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// routes will go here
-
-const router = require("./router");
-const cors = require("cors");
-
-
-dotenv.config();
-
-
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.listen(PORT, async () => {
-  console.log(`server up on port ${PORT}`);
+mongoose.connect("mongodb://localhost:27017/newCollection", {
+useNewUrlParser: true,
+useUnifiedTopology: true
 });
 
-app.use(cors());
+const contactSchema = {
+email: String,
+query: String,
+};
 
+const Contact = mongoose.model("Contact", contactSchema);
 
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/contact", function(req, res){
+	res.render("contact");
+});
+
+app.post("/contact", function (req, res) {
+	console.log(req.body.email);
+const contact = new Contact({
+	email: req.body.email,
+	query: req.body.query,
+});
+contact.save(function (err) {
+	if (err) {
+		throw err;
+	} else {
+		res.render("contact");
+	}
+});
+});
+
+app.listen(3000, function(){
+	console.log("App is running on Port 3000");
+});
