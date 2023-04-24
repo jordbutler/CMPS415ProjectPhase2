055271dd017219ce40d95f3f0632f0e505747e77
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-23 23:05:20 -0500

    Update phase2.js

--- phase2.js
+++ phase2.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://jordbutler:AcidKvvng253@phase2.s5v65hg.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://w0708515:KvvngAcid253@jbdb.wr4nvvi.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 const mongoose = require("mongoose");
 const express = require('express');
@@ -10,28 +10,132 @@ console.log('Server started at http://localhost:' + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const db = mongoose.connect(uri);
     
-    app.get('/', function(req, res) {
-      var outstring = 'Starting... ';
-      res.send(outstring);
-        
-        mongoose.connect(uri).then(() => {
-    console.log("Connected to MongoDB");
+
+ MongoClient.connect(uri)
+  .then(client => {
+    
+    console.log('Connected to Database')
+    const db = client.db('jbdb')
+    const __dirname = '/Users/jorda/Documents/GitHub/CMPS415ProjectPhase2'
+
+    app.post('/rest/ticket/delete', (req, res) => {
+
+      const ticket = req.body.ticketID;
+
+    
+    db.collection('cmps415mongodb').deleteOne({ "ticketID" : ticket }).then(results => {
+      console.log(results)
+      res.redirect('/')
+    })
+    .catch(error => console.error(error))
+    
+      
+    });
+
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+      
+    });
+    
+    app.get('/rest/ticket/list', function(req, res) {
+        db.collection('cmps415mongodb').find().toArray()
+        .then(results => {
+          console.log(results)
+        }).catch(error => console.error(error));
+    const tickets = db.collection('cmps415mongodb').find();
+    res.send('Found this: ' + JSON.stringify(tickets));
+    });
+
+
+
+    app.get('/rest/ticket/:id', function(req, res) {
+      const searchKey = "{ ticketID: '" + req.params.id + "' }";
+      console.log("Looking for: " + searchKey);
+      
+      async function run() {
+        try {
+          
+          const tickets = db.collection('cmps415mongodb');
+    
+          const query = { ticketID: req.params.id };
+      
+          const ticket = await tickets.findOne(query);
+          console.log(ticket);
+          res.send('Found this: ' + JSON.stringify(ticket));  
+      
+        } finally {
+          
+          await client.close();
+        }
+      }
+      run().catch(console.dir);
+      });
+
+      app.post('/rest/ticket/post', (req, res) => {
+        const dt = new Date();
+        db.collection('cmps415mongodb')
+          .insertOne({
+            ticketID : req.body.ticketID,
+            created_at : dt,
+            updated_at : dt,
+            type : req.body.type,
+            subject : req.body.subject,
+            description : req.body.description,
+            priority : req.body.priority,
+            status : req.body.status,
+            recipient : req.body.recipient,
+            submitter : req.body.submitter,
+            asignnee_id : req.body.asignnee_id,
+            followers_id : req.body.followers_id,
+            tags : req.body.tags,
+          })
+          .then(result => {
+            res.redirect('/')
+            res.send('Ticket Created')
+            console.log('Ticket Created')
+          })
+          .catch(error => console.error(error))
+      });
+
+
+      app.post('/rest/ticket/update', (req, res) => {
+                const dt = new Date();
+                db.collection('cmps415mongodb')
+                .findOneAndUpdate(
+                  { "ticketID" : req.body.ticketID },
+                  {
+                    $set: {
+                      updated_at : dt,
+                      type : req.body.type,
+                      subject : req.body.subject,
+                      description : req.body.description,
+                      priority : req.body.priority,
+                      status : req.body.status,
+                      recipient : req.body.recipient,
+                      submitter : req.body.submitter,
+                      asignnee_id : req.body.asignnee_id,
+                      followers_id : req.body.followers_id,
+                      tags : req.body.tags,
+                    },
+                  },
+                  {
+                    upsert: true,
+                  }
+                )
+                .then(result => {
+                  res.redirect('/')
+                  res.send('Ticket Updated')
+                  console.log('Ticket Updated')
+                })
+                .catch(error => console.error(error))
+
+
+      });
+
+     
+
+
   })
-  .catch((err) => {
-    console.log(err);
-  });
-        
- });
-
-app.get('/items', function(req, res) {
-    const database = client.db('sample_airbnb');
-    const cursor = database.collection('account').find();
-  console.log(cursor)
-});
-
-
-
-// Add functions that make DB calls here
+  .catch(console.error);
 
